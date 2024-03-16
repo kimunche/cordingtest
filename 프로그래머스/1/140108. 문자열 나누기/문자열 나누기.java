843a08238229b1283180e2db96842a7a0e208d44
kimunche <90191707+kimunche@users.noreply.github.com>
2024-03-17 00:40:11 +0900

    [level 1] Title: 문자열 나누기, Time: 0.66 ms, Memory: 70.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int solution(String s) {
+        int answer = 0;
+        int diff = 0;
+        int same =0;
+        char x = s.charAt(0);
+
+        for(int i=0; i<s.length(); i++){
+            if(same == diff){
+                answer++;
+                x = s.charAt(i);
+            }
+            if(s.charAt(i) == x){
+                same++;
+            }else{
+                diff++;
+            }
+        }
+
+
+        return answer;
+    }
+}
