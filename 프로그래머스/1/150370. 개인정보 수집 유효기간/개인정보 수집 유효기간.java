28c98a6c51f20fea098153c43a1bfbb64e38a610
kimunche <90191707+kimunche@users.noreply.github.com>
2024-03-18 16:37:05 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 3.10 ms, Memory: 80.7 MB -BaekjoonHub

--- 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
@@ -1,32 +1,38 @@
 import java.util.*;
 
 class Solution {
-    public int[] solution(String today, String[] terms, String[] privacies) {
-        ArrayList<Integer> answer = new ArrayList<>();
-        HashMap<String, Integer> map = new HashMap<>();
-        
-        int date = getDate(today);
+       public int[] solution(String today, String[] terms, String[] privacies) {
+        List<Integer> result = new ArrayList<>();
 
-        for (String s : terms) {
-            String[] term = s.split(" ");
-            map.put(term[0], Integer.parseInt(term[1]));
+        Map<String, Integer> termmap = new HashMap<>();
+
+        long todays = getDate(today);
+
+        for(String t : terms){
+            String[] arr = t.split(" ");
+            termmap.put(arr[0], Integer.parseInt(arr[1]));
         }
-        
-        for (int i = 0; i < privacies.length; i++) {
-            String[] privacy = privacies[i].split(" ");
 
-            if (getDate(privacy[0]) + (map.get(privacy[1]) * 28) <= date) {
-                answer.add(i + 1);
+        for(int i=0; i< privacies.length; i++){
+            String[] pr = privacies[i].split(" "); // [date, term]
+            long date = getDate(pr[0]);
+            long termdate = termmap.get(pr[1])*28;
+            if(date+termdate <= todays){ // 오늘보다 작으면 파기
+                result.add(i+1);
             }
         }
-        return answer.stream().mapToInt(integer -> integer).toArray();
+
+        int[] answer = result.stream().mapToInt(Integer::intValue).toArray();
+
+        return answer;
     }
 
-    private int getDate(String today) {
-        String[] date = today.split("\\.");
-        int year = Integer.parseInt(date[0]);
-        int month = Integer.parseInt(date[1]);
-        int day = Integer.parseInt(date[2]);
-        return (year * 12 * 28) + (month * 28) + day;
+    public long getDate(String date){
+        String[] ymd = date.split("\\.");
+        long year = Long.parseLong(ymd[0])*12*28;
+        long month= Long.parseLong(ymd[1])*28;
+        long day = Long.parseLong(ymd[2]);
+
+        return year+month+day;
     }
 }
