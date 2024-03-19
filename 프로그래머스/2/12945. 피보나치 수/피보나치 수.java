9d7061375e0731d68acafcc49fa6577116607f8b
kimunche <90191707+kimunche@users.noreply.github.com>
2024-03-19 15:05:46 +0900

    [level 2] Title: 피보나치 수, Time: 5.76 ms, Memory: 80.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int n) {
+
+        int mod = 1234567;
+
+        int[] fibo = new int[n+1];
+        fibo[0] =0;
+        fibo[1] =1;
+
+        for(int i =2; i<=n ; i++){
+            fibo[i] = (fibo[i-1] + fibo[i-2])%mod;
+        }
+
+        return fibo[n];
+    }
+}
