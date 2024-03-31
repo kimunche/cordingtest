30a1d553374874083ced7263c7d6aad01d3f1471
kimunche <90191707+kimunche@users.noreply.github.com>
2024-04-01 02:04:24 +0900

    [level 2] Title: 롤케이크 자르기, Time: 198.73 ms, Memory: 196 MB -BaekjoonHub

---
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.java
@@ -0,0 +1,30 @@
+
+import java.util.*;
+
+class Solution {
+    public int solution(int[] topping) {
+        int answer = 0;
+
+        Map<Integer, Integer> map1 = new HashMap<>();
+        Map<Integer, Integer> map2 = new HashMap<>();
+
+        for(int t: topping){
+            map1.put(t, map1.getOrDefault(t,0)+1);
+        }
+
+        for(int t: topping){
+            map2.put(t, map2.getOrDefault(t, 0)+1);
+            if(map1.get(t)-1 ==0 ){
+                map1.remove(t);
+            }else{
+                map1.put(t, map1.get(t)-1);
+            }
+
+            if(map1.size() == map2.size()){
+                answer++;
+            }
+        }
+
+        return answer;
+    }
+}
