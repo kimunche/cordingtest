6252f963b239b112cde3f30c6b5fe290caf5801a
kimunche <90191707+kimunche@users.noreply.github.com>
2024-04-09 01:27:14 +0900

    [level 2] Title: 시소 짝꿍, Time: 0.43 ms, Memory: 75.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class Solution {
+    public long solution(int[] weights) {
+        long answer = 0;
+
+        Map<Double, Integer> map = new HashMap<>();
+
+        Arrays.sort(weights);
+
+        for(int w : weights){
+            double a = w * 1.0;
+            double b = (w * 2.0) / 3.0; // 2/3
+            double c = (w * 2.0) / 4.0; // 2/4
+            double d = (w * 3.0) / 4.0; // 3/4
+
+            if(map.containsKey(a)) answer += map.get(a);
+            if(map.containsKey(b)) answer += map.get(b);
+            if(map.containsKey(c)) answer += map.get(c);
+            if(map.containsKey(d)) answer += map.get(d);
+
+            map.put((w*1.0), map.getOrDefault((w*1.0),0)+1);
+        }
+
+        return answer;
+    }
+}
