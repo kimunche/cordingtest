774591a586191c46e20bb242c544b9fa2a25d023
kimunche <90191707+kimunche@users.noreply.github.com>
2024-04-15 02:06:09 +0900

    [level 2] Title: 디펜스 게임, Time: 0.78 ms, Memory: 77.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/142085. 디펜스 게임/디펜스 게임.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int n, int k, int[] enemy) {
+        int answer = enemy.length;
+        
+        PriorityQueue<Integer> que = new PriorityQueue<>(Collections.reverseOrder());
+        
+        for(int i=0; i<enemy.length; i++){
+            n -= enemy[i];
+            que.add(enemy[i]);
+            
+            if(n<0){
+                if(k>0){
+                    n += que.poll();
+                    k--;
+                }else{
+                    answer =i;
+                    break;
+                }
+            }
+        }
+        
+        return answer;
+    }
+}
