9b22758756a0cb8af76ac376a3f0f617b72afe9e
kimunche <90191707+kimunche@users.noreply.github.com>
2024-05-24 16:39:22 +0900

    [level 2] Title: 올바른 괄호, Time: 16.64 ms, Memory: 53.2 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -3,19 +3,19 @@ import java.util.*;
 
 class Solution {
     boolean solution(String s) {
-
         Stack<Character> stack = new Stack<>();
 
-        for(int i=0; i<s.length(); i++){
-            if(s.charAt(i) == '('){
-                stack.push('(');
-            } else if (s.charAt(i) == ')') {
+        char[] chars = s.toCharArray();
+
+        for(int i=0; i<chars.length; i++){
+            if(chars[i] == '('){
+                stack.push(chars[i]);
+            }else if (chars[i] == ')'){
                 if(stack.isEmpty()){
                     return false;
                 }
                 stack.pop();
             }
-
         }
 
         return stack.isEmpty();
